d83fbd162e8795677195d76a7111e7a03874d8f1
otdoges <otdoges@proton.me>
2025-06-24 13:21:34 -0500

    well im a dumbass

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ export default function RootLayout({
             </ThemeProvider>
           </PostHogProvider>
         </SupabaseProvider>
+        <Analytics />
       </body>
     </html>
   )
