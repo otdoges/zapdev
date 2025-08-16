c593254e9196367401bb3511150a0a1bf8bf2472
otdoges <otdoges@proton.me>
2025-08-15 20:25:48 -0500

    fix: eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "src/lib/webcontainer-templates.ts"] },
+  { ignores: ["dist", "src/lib/webcontainer-templates.ts", "node_modules"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
