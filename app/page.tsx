75103278b2472dc8066277a90b7915ee67e2b607
otdoges <otdoges@proton.me>
2025-09-11 12:18:20 -0500

    removing shi

--- app/page.tsx
+++ app/page.tsx
@@ -239,25 +239,6 @@ export default function HomePage() {
         {/* Header/Navigation Section */}
         <HeaderDropdownWrapper />
 
-        <div className="sticky top-0 left-0 w-full z-[101] bg-background-base header">
-          <div className="absolute top-0 cmw-container border-x border-border-faint h-full pointer-events-none" />
-          <div className="h-1 bg-border-faint w-full left-0 -bottom-1 absolute" />
-          <div className="cmw-container absolute h-full pointer-events-none top-0">
-            <Connector className="absolute -left-[10.5px] -bottom-11" />
-            <Connector className="absolute -right-[10.5px] -bottom-11" />
-          </div>
-
-          <HeaderWrapper>
-            <div className="max-w-[900px] mx-auto w-full flex justify-between items-center">
-              <div className="flex gap-24 items-center">
-                <HeaderBrandKit />
-              </div>
-              <div className="flex gap-8">
-                <UserAuth />
-              </div>
-            </div>
-          </HeaderWrapper>
-        </div>
 
         {/* Hero Section */}
         <section className="overflow-x-clip" id="home-hero">
